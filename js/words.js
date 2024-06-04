64c51e7017d80a14787686b31b5d957e5e8e439b
grsoomro123 <smggroup597@gmail.com>
2024-06-04 22:06:31 +0500

    All Sources Uploaded

---
+++ js/words.js
@@ -0,0 +1,197 @@
+let wordList = [{
+    word: "python",
+    hint: "A high-level programming language."
+},
+{
+    word: "guitar",
+    hint: "A stringed musical instrument."
+},
+{
+    word: "aim",
+    hint: "A goal or objective."
+},
+{
+    word: "venus",
+    hint: "The second planet from the Sun."
+},
+{
+    word: "gold",
+    hint: "A precious yellow metal."
+},
+{
+    word: "Amazon",
+    hint: "A global online shopping website."
+},
+{
+    word: "golang",
+    hint: "A statically typed programming language designed by Google."
+},
+{
+    word: "coding",
+    hint: "The process of writing computer programs."
+},
+{
+    word: "matrix",
+    hint: "A science fiction movie featuring a simulated reality."
+},
+{
+    word: "bugs",
+    hint: "Errors or flaws in a computer program."
+},
+{
+    word: "avatar",
+    hint: "A science fiction film directed by James Cameron."
+},
+{
+    word: "gif",
+    hint: "A format for image files that supports both animated and static images."
+},
+{
+    word: "mental",
+    hint: "Related to the mind or cognitive processes."
+},
+{
+    word: "map",
+    hint: "A visual representation of an area."
+},
+{
+    word: "island",
+    hint: "A piece of land surrounded by water."
+},
+{
+    word: "hockey",
+    hint: "A sport played on ice or a field with sticks and a puck or ball."
+},
+{
+    word: "chess",
+    hint: "A strategic board game played by two players."
+},
+{
+    word: "whatsapp",
+    hint: "A free messaging+calling app."
+},
+{
+    word: "github",
+    hint: "A platform for hosting and collaborating on code."
+},
+{
+    word: "png",
+    hint: "A lossless image file format."
+},
+{
+    word: "silver",
+    hint: "A precious greyish-white metal."
+},
+{
+    word: "mobile",
+    hint: "A portable electronic device for communication."
+},
+{
+    word: "cpu",
+    hint: "A brain of a computer."
+},
+{
+    word: "java",
+    hint: "A popular programming language and computing platform."
+},
+{
+    word: "google",
+    hint: "The world's most popular search engine."
+},
+{
+    word: "venice",
+    hint: "A city known for its canals."
+},
+{
+    word: "excel",
+    hint: "A spreadsheet program by Microsoft."
+},
+{
+    word: "mysql",
+    hint: "An open-source relational database management system."
+},
+{
+    word: "nepal",
+    hint: "A country in South Asia, home to Mount Everest."
+},
+{
+    word: "flute",
+    hint: "A woodwind musical instrument."
+},
+{
+    word: "crypto",
+    hint: "Short for cryptocurrency, a digital or virtual currency."
+},
+{
+    word: "tesla",
+    hint: "A unit of magnetic flux density or an electric car company."
+},
+{
+    word: "mars",
+    hint: "The fourth planet from the Sun."
+},
+{
+    word: "proxy",
+    hint: "An intermediary server separating end users from the websites they browse."
+},
+{
+    word: "email",
+    hint: "A method of exchanging digital messages over the internet."
+},
+{
+    word: "html",
+    hint: "The standard markup language for creating web pages."
+},
+{
+    word: "air",
+    hint: "The invisible gaseous substance surrounding the earth."
+},
+{
+    word: "idea",
+    hint: "A thought or suggestion for a possible course of action."
+},
+{
+    word: "server",
+    hint: "A computer or system that provides resources or services to other computers."
+},
+{
+    word: "svg",
+    hint: "A scalable vector graphics file format."
+},
+{
+    word: "jpeg",
+    hint: "A commonly used method of lossy compression for digital images."
+},
+{
+    word: "search",
+    hint: "To look for information on the internet."
+},
+{
+    word: "key",
+    hint: "A small metal instrument used to open locks."
+},
+{
+    word: "egypt",
+    hint: "A country in North Africa known for its ancient civilization."
+},
+{
+    word: "joker",
+    hint: "A psychological thriller film featuring a DC Comics character."
+},
+{
+    word: "dubai",
+    hint: "A city in the United Arab Emirates known for its modern architecture."
+},
+{
+    word: "photo",
+    hint: "A picture taken with a camera."
+},
+{
+    word: "nile",
+    hint: "The longest river in the world."
+},
+{
+    word: "rain",
+    hint: "Precipitation in the form of water droplets."
+},
+]
